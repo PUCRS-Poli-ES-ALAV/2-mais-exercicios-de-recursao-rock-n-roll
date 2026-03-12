386ee2f4dd924ee6b0e98c5b11b28378d65ed7ff
Henrique Hiller <henriquehiller@gmail.com>
2026-03-11 21:09:38 -0300

    continuacao 2

--- stringDentro.java
+++ stringDentro.java
@@ -1,11 +1,11 @@
 public class stringDentro{
     public static void main(String[] args){
-        System.out.println(findSubStr("rato bateu na mae","na"));
+        System.out.println(findSubStr("rato bateu na mae","nas"));
     }
 
     public static boolean findSubStr(String str, String match){
         if(str.length()<match.length()){return false;}
-        if (str.substring(0, match.length()-1).equals(match)){
+        if (str.substring(0, match.length()).equals(match)){            
             return true;
         }
         return findSubStr(str.substring(1), match);
